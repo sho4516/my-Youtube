92af90012041fbe50821b9ffa85783ada8ad302b
Shobhit <shobhitgoyalg@gmail.com>
2025-02-24 19:28:03 +0530

    sidebar refactored

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -23,6 +23,19 @@ const SideBar = () => {
     { id: 4, icon: "fa-download", label: "Downloads" },
   ];
 
+  const menuItems3 = [
+    { id: 1, icon: "fa-fire", label: "Trending" },
+    { id: 2, icon: "fa-cart-shopping", label: "Shopping" },
+    { id: 3, icon: "fa-music", label: "Music" },
+    { id: 4, icon: "fa-film", label: "Movies" },
+    { id: 4, icon: "fa-star-of-life", label: "Live" },
+    { id: 4, icon: "fa-gamepad", label: "Gaming" },
+    { id: 4, icon: "fa-newspaper", label: "News" },
+    { id: 4, icon: "fa-medal", label: "Sports" },
+    { id: 4, icon: "fa-discourse", label: "Courses" },
+    { id: 4, icon: "fa-podcast", label: "Podcasts" },
+  ];
+
   return (
     <div className="w-[20%] h-[90vh] overflow-y-auto scrollbar-hide">
       <ul className="after:content-[''] after:block after:h-[1px] after:bg-gray-300 after:my-4">
@@ -37,6 +50,9 @@ const SideBar = () => {
         ))}
       </ul>
 
+      <h1 className="ml-3 font-extrabold text-red-700">
+        You<i class="fa-solid fa-arrow-right ml-2 font-light"></i>
+      </h1>
       <ul className="after:content-[''] after:block after:h-[1px] after:bg-gray-300 after:my-4">
         {menuItems2.map((item) => (
           <li
@@ -49,8 +65,11 @@ const SideBar = () => {
         ))}
       </ul>
 
+      <h1 className="ml-3 font-extrabold text-red-700">
+        Explore<i class="fa-solid fa-arrow-right ml-2 font-light"></i>
+      </h1>
       <ul className="after:content-[''] after:block after:h-[1px] after:bg-gray-300 after:my-4">
-        {menuItems2.map((item) => (
+        {menuItems3.map((item) => (
           <li
             key={item.id}
             className="mt-2 border-0 px-4 py-2 flex flex-row justify-start items-center gap-4 rounded-2xl cursor-pointer hover:bg-gray-500 transition"
